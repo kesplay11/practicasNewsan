bc22b38c8c781125cf0f4824d870cc09a554551e
kesplay11 <126436475+kesplay11@users.noreply.github.com>
2025-09-18 11:28:40 +0000

    Añadi las hora funcional

--- src/components/Hora.tsx
+++ src/components/Hora.tsx
@@ -1,9 +1,22 @@
-export default function Hora(){
-    return(
-        <div>
-            <h2 className="hora-h2">
-            10:09:53
-            </h2>  
-        </div> 
-    )
+import  { useState, useEffect } from 'react';
+import moment from 'moment';
+
+export default function Hora() {
+  const [horaActual, setHoraActual] = useState<string>(moment().format('HH:mm:ss'));
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setHoraActual(moment().format('HH:mm:ss'));
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>
+      <h2 className="hora-h2">
+        {horaActual}
+      </h2>
+    </div>
+  );
 }
